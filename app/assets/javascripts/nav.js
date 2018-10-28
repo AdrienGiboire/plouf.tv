e2767b893d0adf9d9f4fbc745976543d1e3e4c39
Adrien Giboire <adrien+github@sent.com>
2018-10-28 19:17:02 +0100

    fix: toggle nav with turbolinks

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,7 +1,12 @@
-window.onload = () => {
+const initEvents = function () {
   // Hamburger to X toggle
   document.querySelector('#nav-toggle').addEventListener('click', function () {
     this.classList.toggle('active')
     toggle(document.querySelector('nav ul.nav-list'))
   })
+}
+
+window.onload = () => {
+  document.addEventListener('turbolinks:load', initEvents)
+  initEvents()
 }
