32375bf36107a2c52a16767e1e1b266b4b8f6b58
Adrien Giboire <adrien+github@sent.com>
2019-04-22 13:19:39 +0200

    update programmation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,10 @@ class PagesController < ApplicationController
     @programs = [
       {
         title: 'Lundi',
-        content: [],
+        content: [
+          '<strong>20h-00h :</strong> Découverte Inked',
+          'https://youtu.be/X08L9kau-Kw'
+        ],
       },
       {
         title: 'Mardi',
@@ -15,40 +18,38 @@ class PagesController < ApplicationController
       },
       {
         title: 'Mercredi',
-        content: [],
+        content: [
+          '<strong>20h :</strong> Cast HL Div 3 Trait Value vs Sauna Tent',
+          '<strong>21h15 :</strong> Cast HL Div 4 PCS-Cronos vs Team Moon Moon',
+        ],
       },
       {
         title: 'Jeudi',
         content: [
-          '<strong>19h-23h :</strong> Vieilleries mais bonneries',
-          "On remonte le temps et on joue à des jeux qui m'ont bercés même si je ne rentrais déjà plus dans un berceau quand j'ai commencé à jouer.",
+          '<strong>19h :</strong> Cast HL Div 3 ProNubs vs Superfreunde Squad',
+          '<strong>21h :</strong> Cast HL Div 5 The Sops vs MortSure',
+          # '<strong>19h-23h :</strong> Vieilleries mais bonneries',
+          # "On remonte le temps et on joue à des jeux qui m'ont bercés même si je ne rentrais déjà plus dans un berceau quand j'ai commencé à jouer.",
         ]
       },
       {
         title: 'Vendredi',
         content: [
-          '<strong>19h-23h :</strong> Diablo III - Saison 15',
-          'On rush la saison 15 en saisonnier nécromancien',
+          '<strong>19h30 :</strong> Cast HL Div 4 Lost in Draft BTW vs The Art of Warfare',
+          '<strong>21h :</strong> Cast HL Div 2 Mondial Murky Oblivion Real Pro Gamer vs Dragon\'s Nest Community',
         ]
       },
       {
         title: 'Samedi',
         content: [
-          '<strong>15h-18h :</strong> Diablo III - Saison 15',
-          'On rush la saison 15 en saisonnier nécromancien',
-          '',
-          '<strong>19h-23h :</strong> Diablo III - Saison 15',
-          'On rush la saison 15 en saisonnier nécromancien',
+          '<strong>20h15 :</strong> Cronos Rumble 12ème édition',
         ]
       },
       {
         title: 'Dimanche',
         content: [
-          '<strong>15h-18h :</strong> Diablo III - Saison 15',
-          'On rush la saison 15 en saisonnier nécromancien',
-          '',
-          '<strong>19h-23h :</strong> Diablo III - Saison 15',
-          'On rush la saison 15 en saisonnier nécromancien',
+          '<strong>20h :</strong> Cast HL Div 1 Agnostic About Minion Genocide vs FieryTale',
+          '<strong>23h :</strong> Cast HL Div SnowStorm vs Without Origins',
         ]
       },
     ]
