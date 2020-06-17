aeefa921c8dca387db030bf9f4eb8b7a8a7fbaf7
Adrien <github@adrien.sent.com>
2020-06-17 23:56:31 +0200

    Use hots_api ruby gem

--- app/controllers/heroes_picker_controller.rb
+++ app/controllers/heroes_picker_controller.rb
@@ -1,5 +1,5 @@
 class HeroesPickerController < ApplicationController
   def index
-    @heroes = HotsAPI::Heroes.get.sort { |a, b| a['name'] <=> b['name'] }
+    @heroes = HotsApi.heroes.to_a.sort { |a, b| a.name <=> b.name }
   end
 end
--- app/lib/hots_api/heroes.rb
+++
@@ -1,7 +0,0 @@
-class HotsAPI::Heroes
-  RESOURCE_NAME = 'heroes'
-
-  def self.get(name=nil)
-    HotsAPI::Client.get(RESOURCE_NAME)
-  end
-end
